1667dd0ca55602ca8f0453d9853a8fbab4d17fe5
Peyman GhaderKourehpaz <poyeshsoftware@gmail.com>
2024-10-15 14:56:38 +0200

    cleanup

--- src/app/services/placement.service.ts
+++ src/app/services/placement.service.ts
@@ -12,36 +12,21 @@ export class PlacementService {
   constructor(private http: HttpClient, private placementRepo: PlacementRepository) {
   }
 
-  fetchPlacements(): Observable<PlacementResponse> {
-    const {filters, pagination, sort} = this.placementRepo.getValue();
-
-    const requestBody = {
-      sort_by: sort.sort_by,
-      sort_order: sort.sort_order,
-      per_page: pagination.perPage,
-      page: pagination.currentPage,
-      paginate: true,
-      key: '',
-      platform: filters.platform,
-      min_total: filters.minTotal,
-      min_invalid_total: filters.minInvalidTotal,
-    };
-
-    // Set loading to true before the request
-    this.placementRepo.setLoading(true);
-
-    return this.http.post<PlacementResponse>(this.apiUrl, requestBody).pipe(
-      tap((response) => {
-        this.placementRepo.updatePlacements(response.data);
-        this.placementRepo.updatePagination({
-          totalPages: response.pagination.total_pages,
-          totalHits: response.pagination.total_hits,
-        });
-      }),
-      finalize(() => {
-        // Set loading to false after the request completes
-        this.placementRepo.setLoading(false);
-      })
-    );
-  }
+  // fetchPlacements(): Observable<PlacementResponse> {
+  //   const {filters, pagination, sort} = this.placementRepo.getValue();
+  //
+  //   const requestBody = {
+  //     sort_by: sort.sort_by,
+  //     sort_order: sort.sort_order,
+  //     per_page: pagination.perPage,
+  //     page: pagination.currentPage,
+  //     paginate: true,
+  //     key: '',
+  //     platform: filters.platform,
+  //     min_total: filters.minTotal,
+  //     min_invalid_total: filters.minInvalidTotal,
+  //   };
+  //
+  //   // Todo: Add loading state and fetch placements and return the response
+  // }
 }
